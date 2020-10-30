8d5699f8641e9b3b77d211b3ee7015670075a418
front-end312 <73707388+front-end312@users.noreply.github.com>
2020-10-30 20:45:58 +0530

    sumbit function button missing

--- form-validator/script.js
+++ form-validator/script.js
@@ -75,3 +75,11 @@ function getFieldName(input) {
 // Event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
+  if(!checkRequired([username, email, password, password2])){
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+  }
+
+});
